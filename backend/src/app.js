1fe999ff2d98066e493725189a5bc36437809607
Jonas <jonas@jonas-UBUNTU-20.04LTS>
2020-05-17 12:45:11 -0300

    Adiciona validações e testes para o app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 const cors = require('cors');
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 /**
  * Rota / Recurso
@@ -35,5 +37,5 @@ app.use(routes);
   */
 
 
-app.listen(3333);
+module.exports = app;
 
